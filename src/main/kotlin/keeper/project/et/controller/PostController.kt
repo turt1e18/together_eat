1800de088110155f7881ca2777046a4754394eee
poeynus <poeynus@gmail.com>
2021-12-28 20:45:28 +0900

    기본적인 설정만 적용

---
+++ src/main/kotlin/keeper/project/et/controller/PostController.kt
@@ -0,0 +1,11 @@
+package keeper.project.et.controller
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.web.bind.annotation.PostMapping
+import org.springframework.web.bind.annotation.RestController
+
+@RestController
+class PostController {
+
+
+}
---
+++ src/main/kotlin/keeper/project/et/service/PostService.kt
@@ -0,0 +1,11 @@
+package keeper.project.et.service
+
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.stereotype.Service
+
+@Service
+class PostService {
+
+
+
+}
