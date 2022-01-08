02cf16e93b0ea38ce14a592dc92f36c34516dde3
poeynus <poeynus@gmail.com>
2022-01-08 14:55:08 +0900

    댓글 입력 및 수정 완료

---
+++ src/main/kotlin/keeper/project/et/dto/request/post/comment/DeleteCommentDTO.kt
@@ -0,0 +1,6 @@
+package keeper.project.et.dto.request.post.comment
+
+data class DeleteCommentDTO (
+    val postNum: Int,
+    val comNum: Int
+        )
---
+++ src/main/kotlin/keeper/project/et/service/CommentService.kt
@@ -0,0 +1,39 @@
+package keeper.project.et.service
+
+import keeper.project.et.dao.CommentDAO
+import keeper.project.et.dto.DataSet
+import keeper.project.et.dto.Message
+import keeper.project.et.dto.request.post.comment.UploadModifyCommentDTO
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.ResponseEntity
+import org.springframework.stereotype.Service
+
+@Service
+class CommentService {
+
+    @Autowired
+    lateinit var commentDAO: CommentDAO
+    val message = { result : String ->
+        listOf(Message(result))
+    }
+
+    fun commentUploadService(uploadModifyCommentDTO: UploadModifyCommentDTO): ResponseEntity<Any> {
+        val resultObject = commentDAO.uploadCommentInfo(uploadModifyCommentDTO)
+
+        return if (resultObject == 1) {
+            ResponseEntity.status(200).body(DataSet(message("success")))
+        } else {
+            ResponseEntity.status(400).body(DataSet(message("fail")))
+        }
+    }
+
+    fun commentModifyService(uploadModifyCommentDTO: UploadModifyCommentDTO): ResponseEntity<Any> {
+        val resultObject = commentDAO.modifyCommentInfo(uploadModifyCommentDTO)
+
+        return if (resultObject == 1) {
+            ResponseEntity.status(200).body(DataSet(message("success")))
+        } else {
+            ResponseEntity.status(400).body(DataSet(message("fail")))
+        }
+    }
+}
